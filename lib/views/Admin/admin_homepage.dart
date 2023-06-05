43a1f202a38bfeba636935ca2392360328104b75
cabrel sianou <sianou93@gmail.com>
2023-06-05 15:34:00 +0200

    add progressbar showing all users comming form firebase

--- lib/views/Admin/admin_homepage.dart
+++ lib/views/Admin/admin_homepage.dart
@@ -1,12 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 import '../../constants/colors.dart';
-class AdminHomePage extends StatelessWidget {
+class AdminHomePage extends StatefulWidget {
   AdminHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<AdminHomePage> createState() => _AdminHomePageState();
+}
+
+class _AdminHomePageState extends State<AdminHomePage> {
+  int _userCount = 0;
   void SignOut(){
     FirebaseAuth.instance.signOut();
+  }
+  void initState() {
+    super.initState();
+    _getUserCount();
+  }
+  Future<void> _getUserCount() async {
+    final usersSnapshot =
+    await FirebaseFirestore.instance.collection('users').get();
+    setState(() {
+      _userCount = usersSnapshot.docs.length;
+    });
   }
 
   @override
@@ -27,7 +47,22 @@ class AdminHomePage extends StatelessWidget {
 
       body: Column(
         children: [
-          Center( child: Text("login",style:  GoogleFonts.poppins( fontWeight: FontWeight.bold),),)
+          CircularPercentIndicator(
+
+            radius: 100.0,
+            lineWidth: 10.0,
+            percent: _userCount / 100,
+            center: Text(
+              "${_userCount}",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+            ),
+            progressColor: Colors.green,
+          ),
+          SizedBox(height: 20),
+          Text(
+            "Nombre Total d'utilisateur : $_userCount",
+            style: TextStyle(fontSize: 18),
+          ),
         ],
       )
     );
